941b8ec8c2e74c931d765dee13a4e0678f58a17e
NemethAdam01 <nemeth.adam@diak.szbi-pg.hu>
2025-03-18 10:42:16 +0100

    for

---
+++ for.js
@@ -0,0 +1,23 @@
+//1
+for (let index = 1; index < 21; index++) {
+    console.log(index, index**2, Math.sqrt(index));
+    
+}
+//2
+for (let index = 0; index < 5; index++) {
+    console.log(("i").repeat(index));
+    
+}
+//3
+for (let index = 99; index > 0; index--) {
+    if (index % 3 == 0) {
+        console.log(index);
+    }
+}
+for (let index = 101; index > 50; index--) {
+    if (index % 5 == 0)
+        {
+         console.log(index * 2)   
+        } 
+    
+}
